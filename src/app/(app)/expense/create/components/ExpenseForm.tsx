b3cc80a2e9547970d31c8261c844bd3f2bf148fa
Vicky <vigneshwaran4817@gmail.com>
2024-09-09 21:00:02 +0530

    Expense form changess

--- src/app/(app)/expense/create/components/ExpenseForm.tsx
+++ src/app/(app)/expense/create/components/ExpenseForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Dropdown from '@/app/(app)/components/form/Dropdown';
-import TextAreaInput from '@/app/(app)/components/form/TextAreaInput';
 import { NOT_SELECTED_CATEGORY_ID, setExpenseCreationForm } from '@/lib/features/expense/expenseSlice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import { Category } from '@/util/AppTypes';
@@ -36,16 +35,16 @@ const ExpenseForm = ({ isFamilyExpense }: { isFamilyExpense: boolean }) => {
   categoryOptions.push({ id: NOT_SELECTED_CATEGORY_ID, displayName: "---", value: "not-selected" })
 
   return (
-    <div className="w-full h-full flex flex-col p-2 my-2 overflow-y-scroll hide-scrollbar">
-      <div className="w-full flex justify-between items-center my-2">
-        <label className="text-[18px] text-light-color-text w-1/4" >Subject</label>
+    <div className="w-full h-full flex flex-col p-2 my-4 overflow-y-scroll hide-scrollbar">
+      <div className="w-full flex justify-between items-center my-4">
+        <label className="text-[18px] text-light-color-text w-1/4" >Name</label>
         <input className="outline-none border-none rounded bg-dark-bg p-2 text-[18px] w-3/4" name="subject" value={creationForm.subject} onChange={handleChange} />
       </div>
-      <div className="w-full flex justify-between items-center my-2">
+      <div className="w-full flex justify-between items-center my-4">
         <label className="text-[18px] text-light-color-text w-1/4" >Date</label>
-        <input type="date" name="date" value={creationForm.date} onChange={handleChange} className="bg-dark-bg p-2 rounded w-3/4" />
+        <input type="datetime-local" name="date" value={creationForm.date} onChange={handleChange} className="bg-dark-bg p-4 rounded w-3/4 max-w-[240px] outline-none" />
       </div>
-      <div className="w-full flex justify-between items-center my-2">
+      <div className="w-full flex justify-between items-center my-4">
         <label className="text-[18px] text-light-color-text w-1/4" >Amount</label>
         <div className="w-3/4 flex items-center justify-between">
           <input className="outline-none border-none rounded bg-dark-bg p-2 text-[18px] w-3/4" name="subject" value={creationForm.subject} onChange={handleChange} />
@@ -61,7 +60,7 @@ const ExpenseForm = ({ isFamilyExpense }: { isFamilyExpense: boolean }) => {
       </div>
       {
         categoryOptions && categoryOptions.length > 0 ?
-          <div className="w-full flex justify-between items-center my-2">
+          <div className="w-full flex justify-between items-center my-4">
             <label className="text-[18px] text-light-color-text w-1/4">Category</label>
             <Dropdown
               className={"bg-dark-bg w-full flex justify-between max-w-[200px] ml-2 p-2"}
@@ -74,9 +73,9 @@ const ExpenseForm = ({ isFamilyExpense }: { isFamilyExpense: boolean }) => {
           </div>
           : <></>
       }
-      <div className="flex w-full justify-between">
+      <div className="flex w-full justify-between my-4">
         <label htmlFor="expense-description" className="text-[18px] text-light-color-text mr-2 sm:mr-none sm:w-1/4">Description</label>
-        <textarea name="description" id="expense-description" className="bg-dark-bg outline-none sm:w-3/4 p-2 rounded resize-none h-[100px]" value={creationForm.description} onChange={e => handleNewValue("description", e.target.value)} />
+        <textarea name="description" id="expense-description" className="bg-dark-bg outline-none sm:w-3/4 p-2 rounded resize-none h-[150px]" value={creationForm.description} onChange={e => handleNewValue("description", e.target.value)} />
       </div>
     </div>
   )
