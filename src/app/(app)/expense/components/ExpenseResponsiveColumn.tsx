cd5a4654244d90a7f576eaeb0d3371c1c5264595
Vicky <vigneshwaran4817@gmail.com>
2024-09-22 18:44:16 +0530

    Expense listing changes

---
+++ src/app/(app)/expense/components/ExpenseResponsiveColumn.tsx
@@ -0,0 +1,10 @@
+import { Expense } from '@/util/AppTypes'
+import React from 'react'
+
+const ExpenseResponsiveColumn = ({ expenses }: { expenses: Expense[] }) => {
+  return (
+    <div>ExpenseResponsiveColumn</div>
+  )
+}
+
+export default ExpenseResponsiveColumn
--- src/app/(app)/expense/page.tsx
+++ src/app/(app)/expense/page.tsx
@@ -2,31 +2,11 @@ import React from 'react'
 import Title from '../components/Title';
 import { getAllExpenses } from '@/app/actions/expense';
 import { NavLink } from '@/app/components/NavLink';
-
-const getDisplayTime = (date: string) => {
-  const dateObj = new Date(date);
-  if (dateObj.toDateString() !== new Date().toDateString()) {
-    const year = new Date().getFullYear() !== dateObj.getFullYear() ? "" : dateObj.getFullYear();
-    return `${dateObj.getDate()} ${dateObj.toLocaleString("en", { "month": "long" })} ${year}`;
-  }
-  return `Today ${dateObj.getHours()}:${dateObj.getMinutes()}`;
-}
+import ExpenseContainer from './components/ExpenseContainer';
 
 const page = async () => {
 
   const expenses = await getAllExpenses();
-
-  const expenseElems = expenses && expenses.map(expense => {
-    return (
-      <tr key={expense.id}>
-        <td className="py-4 text-color-text">{expense.name}</td>
-        <td className="py-4">{expense.amount}</td>
-        <td className="py-4">{getDisplayTime(expense.time)}</td>
-        <td className="py-4">{expense.category?.name || "None"}</td>
-        <td className="py-4">{expense.ownerName}</td>
-      </tr>
-    )
-  });
 
   return (
     <div className="w-full h-full">
@@ -37,20 +17,7 @@ const page = async () => {
         </NavLink>
       </div>
       <div className="w-full h-[calc(100%-55px)] rounded p-2 my-2 bg-dark-bg">
-        <table className="border-collapse w-full text-light-color-text">
-          <thead>
-            <tr className="text-left">
-              <th>Name</th>
-              <th>Amount</th>
-              <th>Date</th>
-              <th>Category</th>
-              <th>Owner</th>
-            </tr>
-          </thead>
-          <tbody>
-            {expenseElems}
-          </tbody>
-        </table>
+        <ExpenseContainer expenses={expenses} />
       </div>
     </div>
   )
