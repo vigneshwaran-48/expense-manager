9261a59624062810dcbbb44bba6e85c80bed0bd7
Vicky <vigneshwaran4817@gmail.com>
2024-10-02 18:14:59 +0530

    Expense role specific restriction and other changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,22 +4,22 @@ import { PRIVATE_ROUTES } from "./util/AppFields";
 
 export async function middleware(request: NextRequest) {
 
-    const path = request.nextUrl.pathname;
-    const matched = PRIVATE_ROUTES.findIndex(matcher => path.startsWith(matcher) && !path.startsWith("/auth"))
-    if (matched > -1) {
+  const path = request.nextUrl.pathname;
+  const matched = PRIVATE_ROUTES.findIndex(matcher => path.startsWith(matcher) && !path.startsWith("/auth"))
+  if (matched > -1) {
 
-        const token = await getToken({ req: request });
+    const token = await getToken({ req: request });
 
-        // if ((!token) && request.nextUrl.pathname === "/") {
-        //     return NextResponse.redirect(new URL("/", process.env.NEXTAUTH_URL));
-        // }
-        if (!token) {
-            return NextResponse.redirect(new URL("/auth/signin?callbackUrl=" + request.url, process.env.NEXTAUTH_URL));
-        }
-
+    // if ((!token) && request.nextUrl.pathname === "/") {
+    //     return NextResponse.redirect(new URL("/", process.env.NEXTAUTH_URL));
+    // }
+    if (!token) {
+      return NextResponse.redirect(new URL("/auth/signin?callbackUrl=" + request.url, process.env.NEXTAUTH_URL));
     }
+
+  }
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 }
